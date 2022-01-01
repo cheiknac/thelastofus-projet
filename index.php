30e75d396b43101819fb2ec8ca64ec119d1e04c2
Cheickna Coulibaly <cheikna.coulibaly@hotmail.fr>
2022-01-01 18:44:54 +0100

    homepage1

--- index.php
+++ index.php
@@ -6,10 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap" rel="stylesheet">
+
+
+
 </head>
 <body>
 
 <div id="left">
+
+    <img src="images/Lucioles_logo.png" width="120px" alt="logo-les lucioles">
 
 
 
@@ -19,14 +30,38 @@
 
 <div id="right">
 
+<nav>
+  <ul>
+   <li><a>The last of us part II</a></li>
+   <li><a>Les lucioles</a></li>
+   <li><a>Shop</a></li>
+   <li><a><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+   <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+   </svg></a></li>
+  </ul>
+</nav>
 
+<div id="small-right">  
 
+   <h1>REJOINDRE LES LUCIOLES</h1>
+   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+   when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 
+   <button type="button">Créer ton profil</button>
 
+   <p>disponible sur<p>
 
+<div id="plateform">
+    <img src="images/1024px-PlayStation_4_logo_and_wordmark.svg.png" width="150px" alt="ps4">
+    <img src="images/ps5-logo-1.jpeg" width="150px" alt="ps5">
+    <img src="images/logo-xbox-series-x.png" width="150px" alt="xbox serie x">
 
-
-</div>
+    </div>
     
+  </div>
+
+  </div>
+
 </body>
 </html>
